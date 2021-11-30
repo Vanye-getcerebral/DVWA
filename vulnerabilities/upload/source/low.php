6cdf94c4b4b6464bf88345d4c5ccab0b82ef9a9f
Vanye Wadawasina <89488932+Vanye-getcerebral@users.noreply.github.com>
2021-11-30 10:38:55 -0800

    Update low.php

--- vulnerabilities/upload/source/low.php
+++ vulnerabilities/upload/source/low.php
@@ -8,7 +8,7 @@ if( isset( $_POST[ 'Upload' ] ) ) {
 	// Can we move the file to the upload folder?
 	if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
 		// No
-		$html .= '<pre>Your image was not uploaded.</pre>';
+		$html .= '<pre>Sorry, Your image was not uploaded.</pre>';
 	}
 	else {
 		// Yes!
